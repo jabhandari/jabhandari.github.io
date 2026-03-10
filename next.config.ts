be65634430ed5bec0130cd01314ff431f8af9de3
jabhandari <jabhandari@myseneca.ca>
2026-03-09 20:09:05 -0400

    remove assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/PersonalWebsite",
-  assetPrefix: "/PersonalWebsite/",
   images: {
     unoptimized: true,
   },
